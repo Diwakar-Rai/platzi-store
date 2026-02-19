4ff49e1ce4348429f2a52675bcb75da34af99611
Diwakar-Rai <rdiwakar230@gmail.com>
2026-02-19 10:20:50 +0530

    added the navbar and login sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
 })
 export class App {}
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { ApiService } from '../../../core/services/api.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   private router = inject(Router);
+  private authService = inject(Auth);
+  isAuthenticated = this.authService.token;
 
   logout() {
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
