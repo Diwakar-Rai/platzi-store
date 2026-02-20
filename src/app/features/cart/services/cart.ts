9087cffa5641bc91c204afeb2bc5a7b5b24bfe0c
Diwakar-Rai <rdiwakar230@gmail.com>
2026-02-20 12:19:22 +0530

    created a cart section

---
+++ src/app/features/cart/services/cart.ts
@@ -0,0 +1,37 @@
+import { computed, Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Cart {
+  private _items = signal<any[]>(JSON.parse(localStorage.getItem('cart') || '[]'));
+  items = computed(() => this._items());
+  totalItems = computed(() => this._items().reduce((acc, item) => acc + item.quantity, 0));
+  totalPrice = computed(() =>
+    this._items().reduce((acc, item) => acc + item.price * item.quantity, 0),
+  );
+
+  addToCart(product: any) {
+    const existing = this._items().find((p) => p.id === product.id);
+    if (existing) {
+      existing.quantity++;
+      this._items.set([...this._items()]);
+    } else {
+      this._items.set([...this._items(), { ...product, quantity: 1 }]);
+    }
+    this.persist();
+  }
+
+  removeFromCart(productId: number) {
+    this._items.set(this._items().filter((p) => p.id !== productId));
+    this.persist();
+  }
+  clearCart() {
+    this._items.set([]);
+    this.persist();
+  }
+
+  private persist() {
+    localStorage.setItem('cart', JSON.stringify(this.items()));
+  }
+}
